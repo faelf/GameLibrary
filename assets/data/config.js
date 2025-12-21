547a8ec5c1339885f73d78344fb1edc59574175c
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-21 01:11:19 +0000

    feat: add getLocale function to determine locale based on currency

--- assets/data/config.js
+++ assets/data/config.js
@@ -2,6 +2,21 @@ export const config = {
   getCurrency() {
     return localStorage.getItem("currency") || "£";
   },
+  getLocale() {
+    const currency = this.getCurrency();
+    switch (currency) {
+      case "£":
+        return "en-GB";
+      case "$":
+        return "en-US";
+      case "€":
+        return "fr-FR";
+      case "R$":
+        return "pt-BR";
+      default:
+        return "en-GB";
+    }
+  },
   getFirstName() {
     return localStorage.getItem("first-name") || "you";
   },
