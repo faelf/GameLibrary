58b3b13f6422590f8b611679b8348ffcc21ee1b4
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-22 23:49:57 +0000

    feat: add formatters utility for date and price formatting

---
+++ assets/utils/formatters.js
@@ -0,0 +1,31 @@
+import { config } from "../data/config.js";
+
+export const formatters = {
+  longDate(dateString) {
+    const locale = config.getLocale();
+    const date = new Date(dateString);
+    return date.toLocaleDateString(locale, {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  },
+  price(price) {
+    const currency = config.getCurrency();
+    const locale = config.getLocale();
+    price = parseFloat(price);
+    return `${price.toLocaleString(locale, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`;
+  },
+  fullPrice(price) {
+    const currency = config.getCurrency();
+    const locale = config.getLocale();
+    price = parseFloat(price);
+    return `${currency}${price.toLocaleString(locale, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`;
+  },
+};
