64a9567955fd60791dca23c5bd353c47d56264b2
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-27 22:00:09 +0000

    feat: add ToastNotification component for user notifications

---
+++ assets/components/ui-toast.js
@@ -0,0 +1,22 @@
+class ToastNotification extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */ `
+    <div class="toast-container position-fixed top-0 end-0 p-3">
+      <div id="app-toast" class="toast" role="alert">
+        <div class="toast-header">
+          <span><!-- Icon will be added here --></span>
+          <strong class="me-auto">
+            <!-- Toast title will be inserted here -->
+          </strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
+        </div>
+        <div class="toast-body" id="toast-body">
+          <!-- Toast message will be inserted here -->
+        </div>
+      </div>
+    </div>
+    `;
+  }
+}
+
+customElements.define("ui-toast", ToastNotification);
