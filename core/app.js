1f386864801ffa2f7b73187411f309415ec86b00
Rafael Ferreira <rafaelf_s@hotmail.com>
2025-12-27 22:02:28 +0000

    refactor: update import paths for pageContent and components in app.js

--- core/app.js
+++ core/app.js
@@ -2,8 +2,8 @@
   Import configuration, page definitions, and Web Components
 */
 import { config } from "../assets/data/config.js";
-import { pageContent } from "../assets/pages/index.js";
-import "../assets/components/components.js";
+import { pageContent } from "../assets/pages/_index.js";
+import "../assets/components/_index.js";
 import { Router } from "./router.js";
 
 /* 
