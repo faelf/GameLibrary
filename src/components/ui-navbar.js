566240a7a527b91ce8d0b60cd7cda5150f805f85
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-06-14 13:31:06 +0000

    refactor: improve ui

--- src/components/ui-navbar.js
+++ src/components/ui-navbar.js
@@ -1,34 +1,37 @@
 class UINavbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */ `
-    <nav class="navbar navbar-expand-md bg-primary">
-      <div class="container">
+    <nav class="navbar navbar-expand-md bg-body-tertiary">
+      <div class="container-xxl">
         <a href="#" class="navbar-brand" data-page="home">Game Library</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
+
+          <ul class="navbar-nav my-2 my-md-0 gap-2 ms-auto">
             <li class="nav-item">
               <a href="#dashboard-page" class="nav-link" data-page-target="dashboard-page">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-svg">
-              <rect width="7" height="9" x="3" y="3" rx="1"/>
-              <rect width="7" height="5" x="14" y="3" rx="1"/>
-              <rect width="7" height="9" x="14" y="12" rx="1"/>
-              <rect width="7" height="5" x="3" y="16" rx="1"/>
-              </svg>
-              <span class="nav-link-text">Dashboard</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-svg">
+                  <rect width="7" height="9" x="3" y="3" rx="1"/>
+                  <rect width="7" height="5" x="14" y="3" rx="1"/>
+                  <rect width="7" height="9" x="14" y="12" rx="1"/>
+                  <rect width="7" height="5" x="3" y="16" rx="1"/>
+                </svg>
+                <span class="nav-link-text">Dashboard</span>
               </a>
             </li>
             <li class="nav-item">
               <a href="#games-list-page" class="nav-link" data-page-target="games-list-page">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-svg">
-              <path d="M12 3v18"/>
-              <rect width="18" height="18" x="3" y="3" rx="2"/>
-              <path d="M3 9h18"/><path d="M3 15h18"/>
-              </svg>
-              <span class="nav-link-text">Games List</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-svg">
+                  <path d="M12 3v18"/>
+                  <rect width="18" height="18" x="3" y="3" rx="2"/>
+                  <path d="M3 9h18"/><path d="M3 15h18"/>
+                </svg>
+                <span class="nav-link-text">Games List</span>
               </a>
             </li>
             <li class="nav-item">
